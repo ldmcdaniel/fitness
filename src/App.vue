d3cece2aad5bf96ab2cf5a10960d617c2f1ffacb
Lucas McDaniel <ldmcdaniel@gmail.com>
2017-01-01 15:18:12 -0600

    Form with select showing different views

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,86 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <h1>Fitness-factory</h1>
+    <form>
+      <select v-model="currentView">
+        <option v-for="(option, key) in options" :value="key">{{option.name}}</option>
+      </select>
+    </form>
+    <div v-show="currentView === 'amrap'">
+      <p>amrap</p>
+    </div>
+    <div v-show="currentView === '5x5'">
+      <p>5x5</p>
+    </div>
+    <div v-show="currentView === 'test'">
+      <p>Judgement</p>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
 
 export default {
+  data() {
+    return {
+      options: {
+        amrap: {
+          name: 'As Many Rounds as Possible',
+        },
+        '5x5': {
+          name: 'Five rounds of five',
+        },
+        test: {
+          name: 'Judgement time',
+        },
+      },
+      exercises: {
+        legs: {
+          barbellSquat: 'Barbell Squat',
+          airSquat: 'Air Squat',
+          deadlift: 'Barbell Deadlift',
+          lunge: 'Barbell Lunge',
+        },
+        push: {
+          pushup: 'Pushup',
+          dip: 'Dip',
+          military: 'Overhead Press',
+          bench: 'Bench Press',
+          incline: 'Incline Bench Press',
+        },
+        pull: {
+          pullup: 'Pullup',
+          row: 'Bent Over Row',
+          upright: 'Upright Barbell Row',
+        },
+        abs: {
+          toesToBar: 'Toes to Bar',
+          knessToChest: 'Knees to Chest',
+          situp: 'Situp',
+        },
+        shoulders: {
+          sideRaise: 'Side Raise',
+        },
+      },
+      currentView: 'amrap',
+      selected: '',
+    };
+  },
+
   name: 'app',
+
+  methods: {
+
+  },
+
+  computed: {
+    // setCurrentView() {
+    //   this.currentView = ;
+    // },
+  },
+
   components: {
-    Hello,
   },
 };
 </script>
